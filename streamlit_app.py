16405131bc6c9b23d9c52d8ba0c8acd871c72d9f
ysp121000 <84312917+ysp121000@users.noreply.github.com>
2024-08-08 13:57:40 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,17 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 file=st.file_uploader('Pick a file')
+
+from langchain.document_loaders import PyMuPDFLoader
+loader=PyMuPDFLoader(file)
+documents=loader.load()
+
+from langchain.text_splitter import CharacterTextSplitter
+
+text_splitter=CharacterTextSplitter(
+    chunk_size=2000,
+    chunk_overlap=500
+)
+docs_split=text_splitter.split_documents(documents)
+print(docs_split[0])
+print(f'{len(documents)} documents have been splitted into{len(docs_split)} chunks')
